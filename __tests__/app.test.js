c18afdd19ae28375dda11696a16f2603e57bd698
CarlieMartece <carliemartece@gmail.com>
2022-08-01 14:38:51 +0100

    add GET api/topics endpoint with requested 200 response

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,27 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../app.js');
+const db = require('../db/connection.js');
+const seed = require('../db/seeds/seed.js');
+const data = require('../db/data/development-data/');
+
+afterAll (() => {
+    return db.end();
+})
+
+beforeEach(() => {
+    return seed(data);
+})
 
 describe('/api/topics', () => {
-    test('GET:200 sends an array of topic objects', () => {
+    test('GET:200 sends an array of topic objects with required properties', () => {
         return request(app)
             .get('/api/topics')
             .expect(200)
+            .then((response) => {
+                expect(response.body.topics).toEqual(expect.any(Array));
+                expect(Object.keys(response.body.topics[0])).toEqual(
+                    expect.arrayContaining(['slug', 'description'])
+                );
+            })
     });
 });
